a645d9623230ae25b6b10644383a8e077d2ef410
Nazeer Akram <104126729+Nazeer-Akram@users.noreply.github.com>
2026-03-03 10:56:19 +0000

    Comparision & DataTypes

---
+++ 01_basics/04_comparisionDatatypes.js
@@ -0,0 +1,27 @@
+//<   :
+//>   :
+//==  :
+//=== :
+
+//Null and Undefined Comparison with 0
+
+//null
+
+let variableNull = null;
+//console.log(variableNull > 0);
+//console.log(variableNull == 0);
+//console.log(variableNull >= 0); //In this condition null becomes 0 and greater than 0
+
+//undefined
+
+let variableUndefined = undefined;
+
+console.log(variableUndefined > 0);
+console.log(variableUndefined == 0);
+console.log(variableUndefined >= 0);
+
+let arrayDataType = []; //: Array Datatype is Objcet
+let objectDataType = {}; //: Object Datatype is Objcet
+let functionDataType = function () {}; //: Function dataype is Function Object
+
+console.log(typeof checkDataType);
