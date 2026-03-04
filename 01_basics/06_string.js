a54f881f1f17e92387480e45ba4016365c1059ee
Nazeer Akram <104126729+Nazeer-Akram@users.noreply.github.com>
2026-03-04 10:51:42 +0000

    String

---
+++ 01_basics/06_string.js
@@ -0,0 +1,17 @@
+let stringSimple =
+  "Hi i am nazeer akram belong from uttar pradesh bareilly 234123"; // The is the simple way of defining the string : Data Type is String
+
+let stringObject = new String(
+  "Hi My name is nazeer akram belong from Uttar Pradesh Bareilly ",
+); //This is the String with object use for manupulating value and data using its methods : Data Tyep is String Objcet.
+
+console.log(stringObject.indexOf("H"));
+
+//substring()
+//sclice ()
+//trim()
+//includes()
+//indexOf()
+//toUpperCase()
+//toLowerCase()
+//length;
