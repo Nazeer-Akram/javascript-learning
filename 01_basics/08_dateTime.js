1277e123f1cd90817f44aa2c2d608dc688b687c1
Nazeer Akram <104126729+Nazeer-Akram@users.noreply.github.com>
2026-03-05 07:50:39 +0000

    08_dateTime

---
+++ 01_basics/08_dateTime.js
@@ -0,0 +1,17 @@
+let learningDate = new Date("2026-03-05T01:30z");
+
+//1. console.log(learningDate); //Date in the Simple Date time Format.
+
+//2. console.log(learningDate.toString()); // In string Format.
+
+//3. console.log(learningDate.toLocaleString()); //Returns Date time in slash format.
+
+//4. console.log(learningDate.toLocaleDateString()); //Returns Only Date in slash format.
+
+//5. console.log(learningDate.toLocaleTimeString()); //Returns Only time in slash format.
+
+//6. console.log(learningDate.getTime()); // Returns date in Milisecond Format.
+
+//7. let convertEpochToTime = new Date(learningDate); // Converting Epoch time (Miliseconds) to date format
+
+//8. console.log(convertEpochToTime.toLocaleString("IN-en"));
