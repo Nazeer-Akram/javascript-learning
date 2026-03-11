05e0f473717798a54780b26c5a793f439931c4ce
Nazeer Akram <104126729+Nazeer-Akram@users.noreply.github.com>
2026-03-11 15:48:49 +0000

    Function with Parameters

---
+++ 03_basics/functions.js
@@ -0,0 +1,39 @@
+//Function with Parameters, argument, console, return
+//console.log(); only prints the funstion value.
+//return; Returns a final result of the function and can be store in a variable.
+
+function callingString(value_1, value_2) {
+  return value_1 + value_2;
+}
+
+//Here in this case we are getting values from the finction to this variable because of return
+let variableFun = callingString(1, 2);
+
+//_____________________________
+
+function returnVariable(value_1, value_2) {
+  let ReturnVar = value_1 + value_2;
+
+  return ReturnVar;
+}
+//console.log("Roll No ", returnVariable(1, 5));
+
+//_____________________________ what if we do not pass any parameters then ?____________________
+
+function withoutParameter(num_1) {
+  return num_1;
+}
+console.log(withoutParameter()); // this returns undefined
+
+//_____________________________ Default Argument value in parameters____________________
+
+function withoutParameter(num_1 = "Nazeer") {
+  return num_1;
+}
+console.log(withoutParameter());
+
+//      summary
+// 1. Empty Parameters ()
+// 2. Parameter with value
+// 3. parameter with default value
+// 4. Differenec btween Console and return
