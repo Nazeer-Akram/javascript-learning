06e091a3dc0b41aa0a0726500e641160a615d8c7
Nazeer Akram <104126729+Nazeer-Akram@users.noreply.github.com>
2026-03-20 14:58:34 +0000

    High Order Array Loops

---
+++ 05_iteration/HighOrderArrayLoops.js
@@ -0,0 +1,39 @@
+// summary
+// 1. forOf Loop
+// 2. forOf Loop with map data type
+// 3. forIn Loop specially designed for Plain Objects
+// 4. foreach loop
+
+const myArray = ["Home", "Office", "Park", "Gym", "School", "Collage"];
+
+const myObj = {
+  name: "Nazeer",
+  age: 25,
+  city: "Delhi",
+};
+
+//-------------------------------------------------------------forOf Loop
+
+// for (const element of myArray) {
+//   console.log(element);
+// }
+
+const myMap = new Map();
+
+myMap.set("Name", "Nazeer");
+myMap.set("Age", "25");
+myMap.set("City", "Jaipur");
+//-------------------------------------------------------------forOf Loop with map data type
+
+// for (const [element, value] of myMap) {
+//   console.log(element, value);
+// }
+
+//-------------------------------------------------------------forIn Loop specially designed for Plain Objects
+for (const key in myObj) {
+  console.log(`The Key is ${key} & the Value is ${myObj[key]}`);
+}
+
+//-------------------------------------------------------------foreach loop
+
+myArray.forEach((iteration) => console.log(iteration));
